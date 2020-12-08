952a4e50369de75781665c3fa263d3f13e39a044
Piotr Rasiński <246764@student.pwr.edu.pl>
2020-12-08 12:57:02 +0100

    Zaokrąglenie rogów, zmiana kolorów na ciemniejsze

--- app/src/main/java/com/example/astroview/DisplayHoroscope.java
+++ app/src/main/java/com/example/astroview/DisplayHoroscope.java
@@ -3,8 +3,12 @@ package com.example.astroview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
 
 public class DisplayHoroscope extends AppCompatActivity {
+
+    private String header = "Okay, here is what we've got for you, ";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +16,14 @@ public class DisplayHoroscope extends AppCompatActivity {
         setContentView(R.layout.activity_display_horoscope);
         String sign = getIntent().getStringExtra("sign");
         String horoscope = getIntent().getStringExtra("horoscope");
+        header += sign;
+        setTextViews(sign, horoscope);
+    }
+
+    public void setTextViews(String sign, String horoscope) {
+        TextView headerTV = findViewById(R.id.headerTextView);
+        headerTV.setText(header);
+        TextView horoscopeTV = findViewById(R.id.horoscopeTextView);
+        horoscopeTV.setText(horoscope);
     }
 }
