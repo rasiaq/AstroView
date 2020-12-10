b8a32935ab6f656dd57b148d49a6cceb8f7bea4c
Piotr Rasiński <246764@student.pwr.edu.pl>
2020-12-10 11:47:32 +0100

    Implemented register activity with logic

--- app/src/main/java/com/example/astroview/Register.java
+++ app/src/main/java/com/example/astroview/Register.java
@@ -1,15 +1,29 @@
 package com.example.astroview;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Register extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     private EditText newEmail;
     private EditText newPassword;
@@ -22,6 +36,9 @@ public class Register extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        mAuth = FirebaseAuth.getInstance();
+
+
         newEmail = findViewById(R.id.newEmail);
         newPassword = findViewById(R.id.newPassword);
         confPassword = findViewById(R.id.confPassword);
@@ -32,20 +49,59 @@ public class Register extends AppCompatActivity {
         confPassword.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
                 String pass1 = newPassword.getText().toString();
                 String pass2 = confPassword.getText().toString();
                 if (!pass1.equals(pass2)) {
                     wrongPasswordTV.setText("Passwords don't match");
+                    signUpBtn.setEnabled(false);
+                    if (!signUpBtn.isEnabled()) signUpBtn.setEnabled(false);
                 } else {
+                    wrongPasswordTV.setText("");
                     signUpBtn.setEnabled(true);
                 }
             }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {}
-
-            @Override
-            public void afterTextChanged(Editable s) {}
         });
+    }
+
+    public void signUp(View view) {
+        String email = newEmail.getText().toString().trim();
+        System.out.println(email);
+        String password = newPassword.getText().toString();
+        System.out.println(password);
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            updateUI();
+                        } else {
+
+                        }
+                    }
+                });
+    }
+
+    public void updateUI() {
+        final Intent intent = new Intent(this, Login.class);
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Registration was successful - you can login now!")
+                .setCancelable(false)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        finish();
+                        startActivity(intent);
+                    }
+                });
+        AlertDialog alert = builder.create();
+        alert.show();
     }
 }
