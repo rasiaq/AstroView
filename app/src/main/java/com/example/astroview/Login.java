7e4a883a3ebd85ac0c3e82b5a309d1f5cc52ecb4
Piotr Rasiński <246764@student.pwr.edu.pl>
2020-12-10 10:58:20 +0100

    Basic login activity

--- app/src/main/java/com/example/astroview/Login.java
+++ app/src/main/java/com/example/astroview/Login.java
@@ -2,10 +2,13 @@ package com.example.astroview;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -28,31 +31,32 @@ public class Login extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
         login = findViewById(R.id.login);
         password = findViewById(R.id.password);
+
     }
 
-//    private void signIn(View view) {
-//        String userLogin = login.getText().toString().trim();
-//        String userPassword = password.getText().toString().trim();
-//
-//        mAuth.createUserWithEmailAndPassword(userLogin, userPassword)
-//                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-//                    @Override
-//                    public void onComplete(@NonNull Task<AuthResult> task) {
-//                        if (task.isSuccessful()) {
-//                            // Sign in success, update UI with the signed-in user's information
-//                            Log.d(TAG, "createUserWithEmail:success");
-//                            FirebaseUser user = mAuth.getCurrentUser();
-//                            updateUI(user);
-//                        } else {
-//                            // If sign in fails, display a message to the user.
-//                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
-//                            Toast.makeText(EmailPasswordActivity.this, "Authentication failed.",
-//                                    Toast.LENGTH_SHORT).show();
-//                            updateUI(null);
-//                        }
-//
-//                        // ...
-//                    }
-//                });
-//    }
+    public void signInUser(View view) {
+
+        String userLogin = login.getText().toString().trim();
+        String userPassword = password.getText().toString().trim();
+
+        mAuth.signInWithEmailAndPassword(userLogin, userPassword)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            updateUI();
+
+                        } else {
+                            Toast.makeText(getBaseContext(), "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    public void updateUI() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
 }
