a141d13c9e6dcb2e209820e20d3840f88e7777c6
Piotr Rasiński <246764@student.pwr.edu.pl>
2020-12-10 11:54:58 +0100

    Implemented exiting app with sign out

--- app/src/main/java/com/example/astroview/MainActivity.java
+++ app/src/main/java/com/example/astroview/MainActivity.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -22,5 +24,10 @@ public class MainActivity extends AppCompatActivity {
     public void getResponse(View v) {
         Intent intent = new Intent(this, GetHoroscope.class);
         startActivity(intent);
+    }
+
+    public void quit(View v) {
+        FirebaseAuth.getInstance().signOut();
+        finish();
     }
 }
